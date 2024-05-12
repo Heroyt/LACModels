28e901b057ab4e3e94a06e80b752a77a93cb72f6
Tomáš Vojík <vojik@wboy.cz>
2024-05-12 11:07:17 +0200

    :bug: Barvicky JS fix

--- Game/Evo5/GameModes/Barvicky.php
+++ Game/Evo5/GameModes/Barvicky.php
@@ -20,80 +20,80 @@ use Lsr\Core\Models\Attributes\PrimaryKey;
 class Barvicky extends AbstractMode implements CustomLoadMode
 {
 
-	use LaserMaxxScores;
+    use LaserMaxxScores;
 
 
-	public string $name = 'Barvičky';
+    public string $name = 'Barvičky';
 
-	/**
-	 * Get a JavaScript file to load which should modify the new game form.
-	 *
-	 * The JavaScript file should contain only one class which extends the CustomLoadMode class.
-	 *
-	 * @return string Script name or empty string
-	 */
-	public function getNewGameScriptToRun() : string {
-		return 'barvicky';
-	}
+    /**
+     * Get a JavaScript file to load which should modify the new game form.
+     *
+     * The JavaScript file should contain only one class which extends the CustomLoadMode class.
+     *
+     * @return string Script name or empty string
+     */
+    public function getNewGameScriptToRun() : string {
+        return 'barvicky';
+    }
 
-	/**
-	 * Modify the game data which should be passed to the load file.
-	 *
-	 * @param array{
-	 *      meta:array<string,string>,
-	 *      players:array{
-	 *        vest:int,
-	 *        name:string,
-	 *        team:string,
-	 *        vip:bool
-	 *      }[],
-	 *      teams:array{
-	 *        key:string,
-	 *        name:string,
-	 *        playerCount:int
-   *      }[],
-   *      hiddenTeams?: string,
-	 *    } $data
-	 *
-	 * @return array{
-	 *      meta:array<string,string>,
-	 *      players:array{
-	 *        vest:int,
-	 *        name:string,
-	 *        team:string,
-	 *        vip:bool
-	 *      }[],
-	 *      teams:array{
-	 *        key:string,
-	 *        name:string,
-	 *        playerCount:int
-   *      }[],
-   *      hiddenTeams?: string,
-	 *    } Modified data
-	 */
-	public function modifyGameDataBeforeLoad(array $data) : array {
-		// Shuffle teams
-      if (isset($data['hiddenTeams']) && $data['hiddenTeams'] === '1') {
-			$teamCount = count($data['teams']);
-			$pKeys = array_keys($data['players']);
-			shuffle($pKeys);
+    /**
+     * Modify the game data which should be passed to the load file.
+     *
+     * @param  array{
+     *      meta:array<string,string>,
+     *      players:array{
+     *        vest:int,
+     *        name:string,
+     *        team:string,
+     *        vip:bool
+     *      }[],
+     *      teams:array{
+     *        key:string,
+     *        name:string,
+     *        playerCount:int
+     *      }[],
+     *      hiddenTeams?: string,
+     *    }  $data
+     *
+     * @return array{
+     *      meta:array<string,string>,
+     *      players:array{
+     *        vest:int,
+     *        name:string,
+     *        team:string,
+     *        vip:bool
+     *      }[],
+     *      teams:array{
+     *        key:string,
+     *        name:string,
+     *        playerCount:int
+     *      }[],
+     *      hiddenTeams?: string,
+     *    } Modified data
+     */
+    public function modifyGameDataBeforeLoad(array $data) : array {
+        // Shuffle teams
+        if (isset($data['hiddenTeams']) && $data['hiddenTeams'] === '1') {
+            $teamCount = count($data['teams']);
+            $pKeys = array_keys($data['players']);
+            shuffle($pKeys);
 
-			foreach ($data['teams'] as $key => $team) {
-				$data['teams'][$key]['playerCount'] = 0;
-			}
+            foreach ($data['teams'] as $key => $team) {
+                $data['teams'][$key]['playerCount'] = 0;
+            }
 
-			$i = 0;
-			foreach ($pKeys as $pKey) {
-				$data['players'][$pKey]['team'] = $data['teams'][$i]['key'];
-				$data['teams'][$i]['playerCount']++;
-				$i = ($i + 1) % $teamCount;
-			}
-		}
+            $i = 0;
+            foreach ($pKeys as $pKey) {
+                $data['players'][$pKey]['team'] = $data['teams'][$i]['key'];
+                $data['teams'][$i]['playerCount']++;
+                $i = ($i + 1) % $teamCount;
+            }
+        }
 
-		// Add starting team color meta
-		foreach ($data['players'] as $player) {
-			$data['meta']['p'.$player['vest'].'-startTeam'] = $player['team'];
-		}
-		return $data;
-	}
+        // Add starting team color meta
+        foreach ($data['players'] as $player) {
+            $data['meta']['p'.$player['vest'].'-startTeam'] = $player['team'];
+        }
+        return $data;
+    }
 }
