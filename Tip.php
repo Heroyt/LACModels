ba2fd35488bba7f69b708733bcc0d2f1ef1e1a9e
Tomáš Vojík <vojik@wboy.cz>
2024-09-22 15:00:05 +0200

    :sparkles: Added tips settings page
    - Can add new tips
    - Can edit tips
    - Can delete tips
    - Tips are now translated separately

--- Tip.php
+++ Tip.php
@@ -6,6 +6,7 @@
 
 namespace App\GameModels;
 
+use App\Core\App;
 use Lsr\Core\Exceptions\ValidationException;
 use Lsr\Core\Models\Attributes\PrimaryKey;
 use Lsr\Core\Models\Model;
@@ -13,9 +14,13 @@ use Lsr\Core\Models\Model;
 #[PrimaryKey('id_tip')]
 class Tip extends Model
 {
-    public const TABLE = 'tips';
+    public const string TABLE = 'tips';
 
     public string $text;
+    public ?string $translations = null;
+
+    /** @var array<string,string>  */
+    private array $translationsParsed;
 
     /**
      * @return string[]
@@ -24,7 +29,7 @@ class Tip extends Model
     public static function shuffledFormatted(): array {
         $formatted = [];
         foreach (self::shuffled() as $tip) {
-            $formatted[] = sprintf(lang('Tip #%d', domain: 'tips'), $tip->id) . ': ' . lang($tip->text, domain: 'tips');
+            $formatted[] = sprintf(lang('Tip #%d', domain: 'tips'), $tip->id) . ': ' . $tip->translate();
         }
         return $formatted;
     }
@@ -46,5 +51,44 @@ class Tip extends Model
      */
     public static function random(): ?Tip {
         return self::query()->orderBy('RAND()')->first();
+    }
+
+    public function getQueryData(): array {
+        $data = parent::getQueryData();
+        $data['translations'] = igbinary_serialize($this->getTranslations());
+        return $data;
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    public function getTranslations(): array {
+        if (!isset($this->translationsParsed)) {
+            if ($this->translations !== null) {
+                $this->translationsParsed = igbinary_unserialize($this->translations);
+            } else {
+                $this->translationsParsed = [];
+            }
+        }
+        return $this->translationsParsed;
+    }
+
+    public function setTranslation(string $lang, string $text): Tip {
+        $this->getTranslations();
+        $this->translationsParsed[$lang] = $text;
+        return $this;
+    }
+
+    public function translate(?string $lang = null): string {
+        if ($lang === null) {
+            $lang = App::getInstance()->translations->getLang();
+        }
+        return $this->getTranslations()[$lang] ?? $this->text;
+    }
+
+    public function jsonSerialize(): array {
+        $data = parent::jsonSerialize();
+        $data['translations'] = $this->getTranslations();
+        return $data;
     }
 }
