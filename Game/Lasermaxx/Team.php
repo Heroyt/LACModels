beab87c69e17f6369464df2521b90b5efef53e58
Tomáš Vojík <vojik@wboy.cz>
2024-12-07 15:11:21 +0100

    :rotating_light: Fixed phpstan warnings

--- Game/Lasermaxx/Team.php
+++ Game/Lasermaxx/Team.php
@@ -5,8 +5,8 @@ namespace App\GameModels\Game\Lasermaxx;
 /**
  * LaserMaxx team model
  *
- * @template G of Game
  * @template P of Player
+ * @template G of Game
  *
  * @extends \App\GameModels\Game\Team<P, G>
  */
