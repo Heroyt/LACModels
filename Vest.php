d4033e0396df5244dabce3c812f2dc8a7ac68e8a
Tomáš Vojík <vojik@wboy.cz>
2025-02-13 11:08:00 +0100

    :sparkles: Added Systems
    - Systems differentiate LG system types (EVO5, EVO6, Laserforce), console IP, mapped directories and available vests
    - Added vest types
    - Systems settings
    - System select in newGame and public newGame (tablet)
    - Updated game loading to support systems

--- Vest.php
+++ Vest.php
@@ -8,11 +8,14 @@ namespace App\GameModels;
 
 use App\GameModels\Game\Enums\VestStatus;
 use App\Models\BaseModel;
+use App\Models\System;
+use App\Models\SystemType;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Lsr\Db\DB;
 use Lsr\ObjectValidation\Exceptions\ValidationException;
 use Lsr\Orm\Attributes\PrimaryKey;
+use Lsr\Orm\Attributes\Relations\ManyToOne;
 use Lsr\Orm\ModelQuery;
 
 #[PrimaryKey('id_vest')]
@@ -21,56 +24,57 @@ class Vest extends BaseModel
     public const string TABLE = 'system_vests';
 
     public string $vestNum;
-    public string $system;
+    #[ManyToOne]
+    public System $system;
     public ?int $gridCol = null;
     public ?int $gridRow = null;
     public VestStatus $status = VestStatus::OK;
     public ?string $info = null;
+    public VestType $type = VestType::VEST;
     public ?DateTimeInterface $updatedAt = null;
 
     /**
-     * @param  string  $system
-     *
      * @return Vest[]
      * @throws ValidationException
      */
-    public static function getForSystem(string $system) : array {
+    public static function getForSystem(string | SystemType | System $system) : array {
         return self::querySystem($system)->get();
     }
 
     /**
-     * @param  string  $system
-     *
      * @return ModelQuery<Vest>
      */
-    public static function querySystem(string $system) : ModelQuery {
+    public static function querySystem(string | SystemType | System $system) : ModelQuery {
+        if ($system instanceof System) {
+            return self::query()->where('id_system = %s', $system->id);
+        }
+        if ($system instanceof SystemType) {
+            $system = $system->value;
+        }
         /** @phpstan-ignore-next-line */
-        return self::query()->where('system = %s', $system);
+        return self::query()
+                   ->where(
+                     'id_system IN %sql',
+                     DB::select(System::TABLE, 'id_system')->where('type = %s', $system)
+                   );
     }
 
-    /**
-     * @param  string  $system
-     *
-     * @return int
-     */
-    public static function getVestCount(string $system) : int {
+    public static function getVestCount(string | SystemType | System $system) : int {
         return self::querySystem($system)->count();
     }
 
-    public static function getGridCols(string $system) : int {
+    public static function getGridCols(string | SystemType | System $system) : int {
         return DB::select(self::TABLE, 'MAX(grid_col)')->where('system = %s', $system)->fetchSingle();
     }
 
-    public static function getGridRows(string $system) : int {
+    public static function getGridRows(string | SystemType | System $system) : int {
         return DB::select(self::TABLE, 'MAX(grid_row)')->where('system = %s', $system)->fetchSingle();
     }
 
     /**
-     * @param  string  $system
-     *
      * @return object{cols:int,rows:int}|null
      */
-    public static function getGridDimensions(string $system) : ?object {
+    public static function getGridDimensions(string | SystemType | System $system) : ?object {
         /* @phpstan-ignore-next-line */
         return DB::select(self::TABLE, 'MAX([grid_col]) as [cols], MAX([grid_row]) as [rows]')->where(
           '[system] = %s',
