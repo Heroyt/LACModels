7f50c3241cad5775749abd3e4abfbf7145d6cd93
Tomáš Vojík <vojik@wboy.cz>
2025-03-09 12:54:35 +0100

    :bug: Fixed loading on TMA vs TMA solo

--- Game/Lasermaxx/GameModes/TmaSolo.php
+++ Game/Lasermaxx/GameModes/TmaSolo.php
@@ -7,6 +7,8 @@
 namespace App\GameModels\Game\Lasermaxx\GameModes;
 
 use App\GameModels\Factory\GameModeFactory;
+use App\GameModels\Game\GameModes\CustomLoadMode;
+use App\Tools\GameLoading\LasermaxxLoadData;
 use Lsr\Orm\Attributes\Factory;
 use Lsr\Orm\Attributes\PrimaryKey;
 
@@ -15,10 +17,22 @@ use Lsr\Orm\Attributes\PrimaryKey;
  */
 #[PrimaryKey('id_mode')]
 #[Factory(GameModeFactory::class)] // @phpstan-ignore-line
-abstract class TmaSolo extends Deathmatch
+abstract class TmaSolo extends Deathmatch implements CustomLoadMode
 {
     use LaserMaxxScores;
 
 
     public string $name = 'T.M.A. - solo';
+
+    public function getNewGameScriptToRun() : string {
+        return '';
+    }
+
+    public function modifyGameDataBeforeLoad(LasermaxxLoadData $loadData, array $data) : LasermaxxLoadData {
+        foreach ($loadData->players as $player) {
+            $player->team = '0'; // Red team
+        }
+        $loadData->soloTeam = 0;
+        return $loadData;
+    }
 }
