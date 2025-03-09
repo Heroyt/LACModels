5f2766d6c6c19632787ec378affb13ce64d880f6
Tomáš Vojík <vojik@wboy.cz>
2025-03-09 12:24:34 +0100

    :sparkles: Gladiator mode improvements

--- Game/Evo6/GameModes/Gladiator.php
+++ Game/Evo6/GameModes/Gladiator.php
@@ -4,11 +4,24 @@ declare(strict_types=1);
 namespace App\GameModels\Game\Evo6\GameModes;
 
 use App\GameModels\Factory\GameModeFactory;
+use App\GameModels\Game\GameModes\CustomLoadMode;
+use App\Tools\GameLoading\LasermaxxLoadData;
 use Lsr\Orm\Attributes\Factory;
 use Lsr\Orm\Attributes\PrimaryKey;
 
 #[PrimaryKey('id_mode'), Factory(GameModeFactory::class)]
-class Gladiator extends Deathmatch
+class Gladiator extends Deathmatch implements CustomLoadMode
 {
     public string $name = 'Gladiator';
+
+    public function getNewGameScriptToRun() : string {
+        return 'gladiator';
+    }
+
+    public function modifyGameDataBeforeLoad(LasermaxxLoadData $loadData, array $data) : LasermaxxLoadData {
+        foreach ($loadData->players as $player) {
+            $player->vip = true;
+        }
+        return $loadData;
+    }
 }
