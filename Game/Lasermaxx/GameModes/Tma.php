0ee3196d0f53c6c998bc5458d1246509a35517f2
Tomáš Vojík <vojik@wboy.cz>
2025-03-09 12:38:20 +0100

    :sparkles: Tma force red color

--- Game/Lasermaxx/GameModes/Tma.php
+++ Game/Lasermaxx/GameModes/Tma.php
@@ -7,6 +7,8 @@
 namespace App\GameModels\Game\Lasermaxx\GameModes;
 
 use App\GameModels\Factory\GameModeFactory;
+use App\GameModels\Game\GameModes\CustomLoadMode;
+use App\Tools\GameLoading\LasermaxxLoadData;
 use Lsr\Orm\Attributes\Factory;
 use Lsr\Orm\Attributes\PrimaryKey;
 
@@ -15,10 +17,21 @@ use Lsr\Orm\Attributes\PrimaryKey;
  */
 #[PrimaryKey('id_mode')]
 #[Factory(GameModeFactory::class)] // @phpstan-ignore-line
-abstract class Tma extends TeamDeathmatch
+abstract class Tma extends TeamDeathmatch implements CustomLoadMode
 {
     use LaserMaxxScores;
 
 
     public string $name = 'T.M.A.';
+
+    public function getNewGameScriptToRun() : string {
+        return '';
+    }
+
+    public function modifyGameDataBeforeLoad(LasermaxxLoadData $loadData, array $data) : LasermaxxLoadData {
+        foreach ($loadData->players as $player) {
+            $player->team = '0'; // Red team
+        }
+        return $loadData;
+    }
 }
